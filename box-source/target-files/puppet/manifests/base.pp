ae1fed48740475e2d75e1e119879b1cc0e151c65
Steve Blamey <steve.blamey@adbury.net>
2019-01-10 11:24:47 +0000

    Add copyright and license

--- box-source/target-files/puppet/manifests/base.pp
+++ box-source/target-files/puppet/manifests/base.pp
@@ -8,10 +8,22 @@
 #
 # Steve Blamey <blameys@blueloop.net> Blueloop Ltd.
 #
-# === Copyright
+# === Copyright and License
 #
-# Copyright 2014 uzERP LLP, unless otherwise noted.
+# Copyright 2018 uzERP LLP, unless otherwise noted.
 #
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Update the apt-cache
 exec { "apt-update":
